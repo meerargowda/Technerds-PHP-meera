f9324f1bf497070a50ae9cd9bcf6d552292cf7fb
meerargowda <meerargowda9@gmail.com>
2023-10-25 16:49:45 +0530

    first commit

---
+++ 25-10-2023/sum.php
@@ -0,0 +1,14 @@
+<?php
+$a=5;
+$b=5.0;
+$type=TRUE;
+$name="meera";
+echo $a+$b;
+var_dump($a);
+echo "<br>";
+var_dump($b);
+echo "<br>";
+var_dump($type);
+echo "<br>";
+var_dump($name);
+?>
---
+++ 25-10-2023/variable.php
@@ -0,0 +1,12 @@
+<?php
+$name="hello world";
+$age=13;
+$b=4.0;
+$type=TRUE;
+echo $name;
+echo "<br>";
+echo $age;
+echo"<br>";
+echo $type;
+echo "<br>";
+?>
