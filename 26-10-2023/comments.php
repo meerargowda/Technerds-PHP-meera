e057319b2da515f19b3d4aa5d5764b3e9bc1eff4
meerargowda <meerargowda9@gmail.com>
2023-10-26 16:04:42 +0530

    php2

---
+++ 26-10-2023/comments.php
@@ -0,0 +1,12 @@
+<?php
+$a="comments in php";
+$s="string type";
+echo"$a<br>";
+//echo"single line comments
+echo("string type"."<br>".$s);
+echo $a,"<br>";
+print"$s<br>";
+print("$s<br>");
+print"comment in php"."<br>".$a;
+echo("the value is".$a."and name is".$s);
+?>
